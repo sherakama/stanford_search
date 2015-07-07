03f12f6bc4643a09609e01cc2cad16fc6e1e9c8d
Shea McKinney <sherakama@gmail.com>
2015-07-06 17:31:21 -0700

    Code formatting updates and change to query parameter path

--- js/google_cse.js
+++ js/google_cse.js
@@ -1,7 +1,15 @@
+/**
+ *
+ * Attach the CSE execute code
+ *
+ **/
+
 jQuery(document).ready(function() {
-    var cx = Drupal.settings.stanford_search.id;
-    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-  });
+  var cx = Drupal.settings.stanford_search.id;
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+});
